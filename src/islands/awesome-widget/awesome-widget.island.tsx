015b31bd2adfcf0f1e4435ec73009357b23a6880
Aaron Davis <aaron@ripemetrics.com>
2024-02-15 13:10:53 -0800

    Refactor island creation in awesome-widget.island.tsx

--- src/islands/awesome-widget/awesome-widget.island.tsx
+++ src/islands/awesome-widget/awesome-widget.island.tsx
@@ -1,14 +1,19 @@
-import { createIslandWebComponent } from 'preact-island'
+import { createIsland, createIslandWebComponent } from 'preact-island'
 
 
 export const AwesomeWidget = () => {
     return <div>awesome widget!</div>
   }
 
-const islandName = 'awesome-widget-island'
+// const islandName = 'awesome-widget-island'
 
 
-const island = createIslandWebComponent(islandName, AwesomeWidget )
+const island = createIsland(AwesomeWidget )
 island.render({
-  selector: islandName,
+  selector: 'awesome-widget-island',
 })
+
+// const island = createIslandWebComponent(islandName, AwesomeWidget )
+// island.render({
+//   selector: islandName,
+// })
