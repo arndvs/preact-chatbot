ce2c5671b163c30a0304140fd2e7870673b955d0
Aaron Davis <aaron@ripemetrics.com>
2024-02-20 18:48:48 -0800

    organizing

--- src/islands/chat-widget/chat-widget.island.tsx
+++ src/islands/chat-widget/chat-widget.island.tsx
@@ -1,101 +1,101 @@
-import { createIslandWebComponent, WebComponentPortal } from 'preact-island'
+import { createIslandWebComponent, WebComponentPortal } from 'preact-island';
 
-import { useState } from 'preact/hooks'
-import Chatbot from "src/components/chat-widget/Chatbot/Chatbot"
-import ActionProvider from "src/components/chat-widget/action-provider"
-import ChatConfig from "src/components/chat-widget/chat-config"
-import MessageParser from "src/components/chat-widget/message-parser"
-import * as styles from './chat-widget.css'
-import { useWebComponentEvents } from "src/hooks/useWebComponentEvents"
-import { FC } from 'preact/compat'
-import cx from 'clsx'
-import { Box, Button, Text } from '../../components/_shared'
-import { ChatIcon } from 'src/assets/chat-icon'
-import { ChevronDownIcon } from 'src/assets/chevron-down-icon'
-import { XMarkIcon } from 'src/assets/x-mark-icon'
+import cx from 'clsx';
+import { FC } from 'preact/compat';
+import { useState } from 'preact/hooks';
+import { ChatIcon } from 'src/assets/chat-icon';
+import { XMarkIcon } from 'src/assets/x-mark-icon';
+import { Box } from 'src/components/_shared';
+import ActionProvider from 'src/components/chat-widget/action-provider';
+import ChatConfig from 'src/components/chat-widget/chat-config';
+import Chatbot from 'src/components/chat-widget/Chatbot/Chatbot';
+import MessageParser from 'src/components/chat-widget/message-parser';
+import { useWebComponentEvents } from 'src/hooks/useWebComponentEvents';
+import * as styles from 'src/styles/chat-widget.css';
 
 const Portalize: FC<{ name: string; parent: string }> = ({
-    children,
-    name,
-    parent,
-  }) => {
-    useWebComponentEvents(name, parent)
+  children,
+  name,
+  parent
+}) => {
+  useWebComponentEvents(name, parent);
 
-    // @ts-ignore types are wrong
-    return <WebComponentPortal name={name}>{children}</WebComponentPortal>
-  }
+  // @ts-ignore types are wrong
+  return <WebComponentPortal name={name}>{children}</WebComponentPortal>;
+};
 
+export const ChatWidget = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  useWebComponentEvents(islandName);
 
-
-export const ChatWidget  = () => {
-
-
-    const [isOpen, setIsOpen] = useState(false)
-    useWebComponentEvents(islandName)
-
-    return  (
+  return (
     <>
-
-
-    <div>
-    <button
-      className={styles.chatBubbleButton}
-
-      onClick={() => setIsOpen(!isOpen)}
-      data-testid="chat-bubble-button"
-    >
-      <div className={styles.chatBubbleButtonContent}>
-  {!isOpen ? ( <ChatIcon
+      <div>
+        <button
+          className={styles.chatBubbleButton}
+          onClick={() => setIsOpen(!isOpen)}
+          data-testid="chat-bubble-button"
+        >
+          <div className={styles.chatBubbleButtonContent}>
+            {!isOpen ? (
+              <ChatIcon
+                className={styles.chatBubbleButtonImage}
+                aria-hidden="true"
+              />
+            ) : (
+              <>
+                <XMarkIcon
                   className={styles.chatBubbleButtonImage}
                   aria-hidden="true"
-                />) : (
-                    <>
-                    <XMarkIcon
-                    className={styles.chatBubbleButtonImage}
-                    aria-hidden="true"
-                    />
-                    </>
+                />
+              </>
+            )}
+          </div>
+        </button>
 
-                )}
-
-  </div>
-    </button>
-
-    {isOpen && (
-      <Portalize name="chat-dialog" parent={islandName}>
-        <Box
-          data-testId="modal-content"
-          className={cx(styles.chatDialog, isOpen && styles.chatDialogVisible)}
-        >
-          <Chatbot
-            config={ChatConfig}
-            messageParser={MessageParser}
-            actionProvider={ActionProvider}
-          />
-
-        </Box>
-      </Portalize>
-    )}
-    {isOpen && (
-      <Portalize name="chat-dialog-dimmer" parent={islandName}>
-        <Box
-          data-testId="modal-dimmer"
-          className={cx(styles.chatDialogDimmer, isOpen && styles.chatDialogDimmerVisible)}
-          onClick={() => setIsOpen(false)}
-        />
-      </Portalize>
-    )}
-  </div>
-
-
+        {isOpen && (
+          <Portalize
+            name="chat-dialog"
+            parent={islandName}
+          >
+            <Box
+              data-testId="modal-content"
+              className={cx(
+                styles.chatDialog,
+                isOpen && styles.chatDialogVisible
+              )}
+            >
+              <Chatbot
+                config={ChatConfig}
+                messageParser={MessageParser}
+                actionProvider={ActionProvider}
+              />
+            </Box>
+          </Portalize>
+        )}
+        {isOpen && (
+          <Portalize
+            name="chat-dialog-dimmer"
+            parent={islandName}
+          >
+            <Box
+              data-testId="modal-dimmer"
+              className={cx(
+                styles.chatDialogDimmer,
+                isOpen && styles.chatDialogDimmerVisible
+              )}
+              onClick={() => setIsOpen(false)}
+            />
+          </Portalize>
+        )}
+      </div>
     </>
-    )
-  }
+  );
+};
 
+const islandName = 'chat-widget-island';
 
-const islandName = 'chat-widget-island'
-
-const island = createIslandWebComponent(islandName, ChatWidget )
+const island = createIslandWebComponent(islandName, ChatWidget);
 island.render({
-  selector: islandName,
-})
+  selector: islandName
+});
--- src/islands/subscribe-email/subscribe-email.island.tsx
+++ src/islands/subscribe-email/subscribe-email.island.tsx
@@ -1,34 +1,34 @@
-import '../../styles/reset.css'
+import 'src/styles/reset.css';
 
-import { createIslandWebComponent, WebComponentPortal } from 'preact-island'
-import { useState } from 'preact/hooks'
-import cx from 'clsx'
-import { Box, Button, Text } from '../../components/_shared'
-import * as styles from './subscribe-email.css'
-import { FC } from 'preact/compat'
-import { useWebComponentEvents } from 'src/hooks/useWebComponentEvents'
+import { createIslandWebComponent, WebComponentPortal } from 'preact-island';
+import { useState } from 'preact/hooks';
+import cx from 'clsx';
+import { Box, Button, Text } from '../../components/_shared';
+import * as styles from 'src/styles/subscribe-email.css';
+import { FC } from 'preact/compat';
+import { useWebComponentEvents } from 'src/hooks/useWebComponentEvents';
 
-const islandName = 'subscribe-email-island'
+const islandName = 'subscribe-email-island';
 
 const Portalize: FC<{ name: string; parent: string }> = ({
   children,
   name,
-  parent,
+  parent
 }) => {
-  useWebComponentEvents(name, parent)
+  useWebComponentEvents(name, parent);
 
   // @ts-ignore types are wrong
-  return <WebComponentPortal name={name}>{children}</WebComponentPortal>
-}
+  return <WebComponentPortal name={name}>{children}</WebComponentPortal>;
+};
 
 export const SubscribeEmail = ({
-  backgroundColor,
+  backgroundColor
 }: {
-  backgroundColor?: string
+  backgroundColor?: string;
 }) => {
-  const [isOpen, setIsOpen] = useState(false)
+  const [isOpen, setIsOpen] = useState(false);
 
-  useWebComponentEvents(islandName)
+  useWebComponentEvents(islandName);
 
   return (
     <div>
@@ -42,7 +42,10 @@ export const SubscribeEmail = ({
       </button>
 
       {isOpen && (
-        <Portalize name="starter-modal" parent={islandName}>
+        <Portalize
+          name="starter-modal"
+          parent={islandName}
+        >
           <Box
             data-testId="modal-content"
             className={cx(styles.modal, isOpen && styles.modalVisible)}
@@ -52,14 +55,20 @@ export const SubscribeEmail = ({
               src="https://github.com/mwood23/preact-island/raw/master/docs/preact-island.svg"
             />
             <Text>Portals work with web component islands too!</Text>
-            <Button className="cta_button" onClick={() => setIsOpen(false)}>
+            <Button
+              className="cta_button"
+              onClick={() => setIsOpen(false)}
+            >
               close
             </Button>
           </Box>
         </Portalize>
       )}
       {isOpen && (
-        <Portalize name="starter-dimmer" parent={islandName}>
+        <Portalize
+          name="starter-dimmer"
+          parent={islandName}
+        >
           <Box
             data-testId="modal-dimmer"
             className={cx(styles.dimmer, isOpen && styles.dimmerVisible)}
@@ -68,10 +77,10 @@ export const SubscribeEmail = ({
         </Portalize>
       )}
     </div>
-  )
-}
+  );
+};
 
-const island = createIslandWebComponent(islandName, SubscribeEmail)
+const island = createIslandWebComponent(islandName, SubscribeEmail);
 island.render({
-  selector: islandName,
-})
+  selector: islandName
+});
