9bfeac8f52ae49dba6430b55c9fccf28668d7a89
Aaron Davis <aaron@ripemetrics.com>
2024-02-22 15:35:27 -0800

    Add initialProps to island.render()

--- src/islands/ripechat/ripechat.island.tsx
+++ src/islands/ripechat/ripechat.island.tsx
@@ -11,5 +11,6 @@ export const Ripechat = () => {
 
 const island = createIslandWebComponent(islandName, Ripechat);
 island.render({
-  selector: islandName
+  selector: islandName,
+  initialProps: {}
 });
