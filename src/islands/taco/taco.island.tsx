5d963dbb11a60e1cdb3a38da190dee8b154f9256
Aaron Davis <aaron@ripemetrics.com>
2024-02-22 16:34:39 -0800

    Update Taco component styling

--- src/islands/taco/taco.island.tsx
+++ src/islands/taco/taco.island.tsx
@@ -7,13 +7,14 @@ import { useWebComponentEvents } from 'src/hooks/useWebComponentEvents';
 
 const islandName = 'taco-island';
 
-export const Taco = () => {
+export const Taco = ({ backgroundColor }: { backgroundColor?: string }) => {
   useWebComponentEvents(islandName);
 
   return (
     <div>
       <button
         className={styles.button}
+        style={{ backgroundColor: backgroundColor }}
         data-testid="Taco"
       >
         Taco
--- src/styles/taco.css.ts
+++ src/styles/taco.css.ts
@@ -2,7 +2,7 @@ import { style } from '@vanilla-extract/css';
 
 export const button = style({
   border: 'none',
-  backgroundColor: '#294eab',
+  backgroundColor: 'orange',
   borderRadius: '5px',
   padding: '10px',
   fontWeight: 'bold',
