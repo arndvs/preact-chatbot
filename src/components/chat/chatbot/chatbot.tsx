4a2ba2565813b2f0e44362bb3e0874709826cc3a
andrew carrigan <andrew@ripemetrics.com>
2024-03-04 09:08:46 -0800

    Update color theme and add Pusher and Laravel Echo dependencies

--- src/components/chat/chatbot/chatbot.tsx
+++ src/components/chat/chatbot/chatbot.tsx
@@ -10,6 +10,10 @@ import {
 } from 'src/actions/chatbot/chatbot-config-utils';
 import { createChatBotMessage } from 'src/actions/chatbot/chatbot-message-utils';
 import ChatbotError from './chatbot-error';
+import Pusher from 'pusher-js';
+import Echo from 'laravel-echo';
+import { useEffect, useState } from 'preact/hooks';
+import config from '../../../playwright.config';
 
 const Chatbot = ({
   actionProvider,
@@ -45,6 +49,66 @@ const Chatbot = ({
     runInitialMessagesWithHistory,
     ...rest
   });
+  const [echo, setEcho] = useState<Echo | null>(null);
+  const [pusher, setPusher] = useState<Pusher | null>(null);
+
+  useEffect(() => {
+    const pusherConfig = {
+      key: 'ba4d144ab20fb212f010',
+      cluster: 'us3'
+    };
+    // console.log('Pusher config:', pusherConfig);
+
+    Pusher.logToConsole = true;
+    // console.log('Pusher key:');
+    // if (!token) return;
+    try {
+      setPusher(
+        new Pusher(pusherConfig.key, { cluster: pusherConfig.cluster })
+      );
+      if (!echo || echo === undefined) {
+        const newEcho = new Echo({
+          broadcaster: 'pusher',
+          key: pusherConfig.key,
+          cluster: pusherConfig.cluster,
+          forceTLS: true,
+          authEndpoint: `https://api.rmdevs.com/api/broadcasting/reputation`
+        });
+        setEcho(newEcho);
+        console.log('Pusher client created');
+        console.log('New Echo', newEcho);
+        // console.log('New Echo', window.Echo);
+        // console.log('Pusher client created');
+      } else {
+        console.log('Pusher client already exists');
+      }
+    } catch (error) {
+      console.error('Error creating Pusher client:', error);
+    }
+
+    const subscription = `chat-stream-12-`;
+    // console.log('user channel ID', subscription);
+
+    if (
+      echo !== undefined &&
+      echo !== null
+      // userChannels.length > 0
+    ) {
+      // console.log('user channel is in if:', subscription);
+      // echo.private(subscription).listenToAll((e, data) => {
+      //   console.log('chatbot response:', e);
+      //   // console.log('text:', data.text);
+      //   if (data?.completed === false) {
+      //     setAiUserTestResponse((prevResponse) => prevResponse + data.text);
+      //   }
+      // });
+    }
+
+    return () => {
+      // Clean up subscription that is not an array
+      // subscription.unsubscribe();
+    };
+  }, []);
 
   if (configurationError) {
     return <ChatbotError message={configurationError} />;
