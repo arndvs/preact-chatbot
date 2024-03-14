76b232470b6b6303f266585d7aea40d77e06aaf7
Aaron Davis <aaron@ripemetrics.com>
2024-03-14 08:50:22 -0700

    Refactor client chat button island to use useRef for element appending

--- src/islands/client-chat-button/client-chat-button.island.tsx
+++ src/islands/client-chat-button/client-chat-button.island.tsx
@@ -5,8 +5,7 @@ import { createIslandWebComponent } from 'preact-island';
 
 import { useWebComponentEvents } from 'src/hooks/useWebComponentEvents';
 import ClientChatButtonComponent from 'src/components/client-components/client-chat-button-component';
-import { useDynamicWebIsland } from 'src/hooks/useDynamicWebComponent';
-import { useEffect } from 'preact/hooks';
+import { useEffect, useRef } from 'preact/hooks';
 
 const islandName = 'client-chat-button-island';
 
@@ -16,32 +15,42 @@ const storeId = document.currentScript?.getAttribute('data-store-id') as
 
 export const ClientChatButtonIsland = () => {
   useWebComponentEvents(islandName);
-  //   useDynamicWebIsland(islandName);
+
+  // useRef to ensure element is appended only once
+  const islandAppended = useRef(false);
 
   useEffect(() => {
-    const appendElement = () => {
-      // Check if the element with the specified name already exists
-      if (!document.querySelector(islandName)) {
-        // If it doesn't exist, create a new element
-        const element = document.createElement(islandName);
-        // Find the <body> tag
-        const body = document.querySelector('body');
-        // Check if the body exists (DOM might not be ready yet)
-        if (body && body.lastElementChild) {
-          // Insert the newly created element right before the closing </body> tag
-          body.insertBefore(element, body.lastElementChild.nextSibling);
-        } else {
-          // If body is not found, log an error
-          console.error('Body element not found. Make sure the DOM is ready.');
+    // Append element only once
+    if (!islandAppended.current) {
+      const appendElement = () => {
+        // Check if the element with the specified name already exists
+        if (!document.querySelector(islandName)) {
+          // If it doesn't exist, create a new element
+          const element = document.createElement(islandName);
+          // Find the <body> tag
+          const body = document.querySelector('body');
+          // Check if the body exists (DOM might not be ready yet)
+          if (body && body.lastElementChild) {
+            // Insert the newly created element right before the closing </body> tag
+            body.insertBefore(element, body.lastElementChild.nextSibling);
+          } else {
+            // If body is not found, log an error
+            console.error(
+              'Body element not found. Make sure the DOM is ready.'
+            );
+          }
         }
-      }
-    };
+      };
 
-    // Execute appendElement function when DOM is ready
-    if (document.readyState === 'loading') {
-      document.addEventListener('DOMContentLoaded', appendElement);
-    } else {
-      appendElement();
+      // Execute appendElement function when DOM is ready
+      if (document.readyState === 'loading') {
+        document.addEventListener('DOMContentLoaded', appendElement);
+      } else {
+        appendElement();
+      }
+
+      // Mark as appended to prevent further appending
+      islandAppended.current = true;
     }
   }, [islandName]);
 
