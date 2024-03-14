78b4fdf556d5aa32012e35b1a3df50178184bd70
Aaron Davis <aaron@ripemetrics.com>
2024-03-14 08:54:35 -0700

    Add useRef to ensure element is appended only once

--- src/hooks/useDynamicWebComponent.tsx
+++ src/hooks/useDynamicWebComponent.tsx
@@ -1,5 +1,5 @@
 // src/hooks/useDynamicWebComponent.js
-import { useEffect } from 'preact/hooks';
+import { useEffect, useRef } from 'preact/hooks';
 
 /**
  * If there is not an element with the island name, automatically insert islandName
@@ -8,10 +8,41 @@ import { useEffect } from 'preact/hooks';
  */
 
 export const useDynamicWebIsland = (islandName: string) => {
+  // useRef to ensure element is appended only once
+  const islandAppended = useRef(false);
+
   useEffect(() => {
-    if (!document.querySelector(islandName)) {
-      const element = document.createElement(islandName);
-      document.body.appendChild(element);
+    // Append element only once
+    if (!islandAppended.current) {
+      const appendElement = () => {
+        // Check if the element with the specified name already exists
+        if (!document.querySelector(islandName)) {
+          // If it doesn't exist, create a new element
+          const element = document.createElement(islandName);
+          // Find the <body> tag
+          const body = document.querySelector('body');
+          // Check if the body exists (DOM might not be ready yet)
+          if (body && body.lastElementChild) {
+            // Insert the newly created element right before the closing </body> tag
+            body.insertBefore(element, body.lastElementChild.nextSibling);
+          } else {
+            // If body is not found, log an error
+            console.error(
+              'Body element not found. Make sure the DOM is ready.'
+            );
+          }
+        }
+      };
+
+      // Execute appendElement function when DOM is ready
+      if (document.readyState === 'loading') {
+        document.addEventListener('DOMContentLoaded', appendElement);
+      } else {
+        appendElement();
+      }
+
+      // Mark as appended to prevent further appending
+      islandAppended.current = true;
     }
   }, [islandName]);
 };
--- src/islands/client-chat-button/client-chat-button.island.tsx
+++ src/islands/client-chat-button/client-chat-button.island.tsx
@@ -16,44 +16,6 @@ const storeId = document.currentScript?.getAttribute('data-store-id') as
 export const ClientChatButtonIsland = () => {
   useWebComponentEvents(islandName);
 
-  // useRef to ensure element is appended only once
-  const islandAppended = useRef(false);
-
-  useEffect(() => {
-    // Append element only once
-    if (!islandAppended.current) {
-      const appendElement = () => {
-        // Check if the element with the specified name already exists
-        if (!document.querySelector(islandName)) {
-          // If it doesn't exist, create a new element
-          const element = document.createElement(islandName);
-          // Find the <body> tag
-          const body = document.querySelector('body');
-          // Check if the body exists (DOM might not be ready yet)
-          if (body && body.lastElementChild) {
-            // Insert the newly created element right before the closing </body> tag
-            body.insertBefore(element, body.lastElementChild.nextSibling);
-          } else {
-            // If body is not found, log an error
-            console.error(
-              'Body element not found. Make sure the DOM is ready.'
-            );
-          }
-        }
-      };
-
-      // Execute appendElement function when DOM is ready
-      if (document.readyState === 'loading') {
-        document.addEventListener('DOMContentLoaded', appendElement);
-      } else {
-        appendElement();
-      }
-
-      // Mark as appended to prevent further appending
-      islandAppended.current = true;
-    }
-  }, [islandName]);
-
   return (
     <ClientChatButtonComponent
       islandName={islandName}
