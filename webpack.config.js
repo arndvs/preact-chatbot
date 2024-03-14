74903ed56b79ce5d739f4b00877b06bd8e94ed6a
Aaron Davis <aaron@ripemetrics.com>
2024-03-13 18:50:13 -0700

    Update island name in preview

--- webpack.config.js
+++ webpack.config.js
@@ -241,7 +241,7 @@ module.exports = ({ dev, prod }) => {
           .map((island) => {
             return `<div class="preview">
               <${island.elementName}></${island.elementName}>
-              <span style="position: absolute; top: -18px; font-size: 18px; font-weight: bold, color: rgba(0, 0, 0, 0.5);">${island.name} </span>
+              <span style="position: absolute; font-size: 18px; font-weight: bold, color: rgba(0, 0, 0, 0.5);">${island.name} Island</span>
             </div>`;
           })
           .join('')}
