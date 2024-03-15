4e97ca69766781d81ef09f7ce1fc842232b7e9db
Aaron Davis <aaron@ripemetrics.com>
2024-03-14 17:48:29 -0700

    Update chat modal styles for different screen sizes

--- src/components/chat/chatbot/chat-modal.tsx
+++ src/components/chat/chatbot/chat-modal.tsx
@@ -38,12 +38,12 @@ const ChatModal = ({ isOpen, setIsOpen, islandName }: ChatModalProps) => {
           <Box
             data-testId="overlay-content"
             className={useClassNames(
-              isOpen && 'hidden sm:block',
               'z-[888889] border-none fixed flex flex-col justify-between shadow-custom rounded-lg overflow-hidden bg-white',
-              'lg:w-[28rem] lg:max-h-[80vh] lg:bottom-4 lg:right-4', // Large screens
-              'md:w-[24rem] md:max-h-[70vh] md:bottom-8 md:right-8', // Medium screens
-              'sm:w-[20rem] sm:max-h-[60vh] sm:bottom-12 sm:right-12', // Small screens
-              'max-w-full max-h-full inset-0' // Extra small screens (mobile)
+              'lg:w-[28rem] lg:max-h-[80vh] lg:bottom-20 lg:right-4', // Large screens
+              'md:w-[24rem] md:max-h-[70vh] md:bottom-20 md:right-4', // Medium screens
+              'sm:w-[20rem] sm:max-h-[60vh] sm:bottom-20 sm:right-4', // Small screens
+              'xs:w-full xs:h-full xs:bottom-0 xs:right-0', // Extra small screens (mobile)
+              'max-w-full max-h-full bottom-0 right-0' // Extra small screens (mobile)
             )}
           >
             {/* Initialize Chatbot, ChatbotConfig, MessageParser, ActionProvider */}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: ['./src/**/*.{ts,tsx}'],
   theme: {
+    screens: {
+      xs: '475px',
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px'
+    },
     extend: {
       boxShadow: {
         custom:
