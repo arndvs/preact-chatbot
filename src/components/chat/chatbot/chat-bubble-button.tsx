cd8aac890e72bc91fd3c991fc4027bf2a9baa77d
Aaron Davis <aaron@ripemetrics.com>
2024-03-21 13:19:15 -0700

    Refactor chat bubble button styling

--- src/components/chat/chatbot/chat-bubble-button.tsx
+++ src/components/chat/chatbot/chat-bubble-button.tsx
@@ -10,9 +10,9 @@ interface ChatBubbleButtonProps {
 const ChatBubbleButton = ({ isOpen, setIsOpen }: ChatBubbleButtonProps) => {
   const { brandColor } = useChatbotContext();
   return (
-    <>
+    <div className="fixed bottom-4 right-4">
       <button
-        className="inline-flex items-center justify-center fixed z-[888888] text-white rounded-full shadow-custom h-12 w-12 cursor-pointer hover:duration-200 hover:scale-105 bottom-4 right-4 hover:shadow-md hover:contrast-150"
+        className="inline-flex items-center justify-center  z-[888888] text-white rounded-full shadow-custom h-12 w-12 cursor-pointer hover:duration-200 hover:scale-105  hover:shadow-md hover:contrast-150"
         onClick={() => setIsOpen(!isOpen)}
         style={{
           backgroundColor: brandColor ?? '#e5e5e5'
@@ -34,7 +34,7 @@ const ChatBubbleButton = ({ isOpen, setIsOpen }: ChatBubbleButtonProps) => {
           )}
         </div>
       </button>
-    </>
+    </div>
   );
 };
 
