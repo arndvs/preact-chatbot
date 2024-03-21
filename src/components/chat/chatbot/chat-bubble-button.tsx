f762ef5663e5694862954d0eeb0fc2e874cb939c
Aaron Davis <aaron@ripemetrics.com>
2024-03-21 13:09:42 -0700

    Refactor chat bubble button component

--- src/components/chat/chatbot/chat-bubble-button.tsx
+++ src/components/chat/chatbot/chat-bubble-button.tsx
@@ -11,32 +11,30 @@ const ChatBubbleButton = ({ isOpen, setIsOpen }: ChatBubbleButtonProps) => {
   const { brandColor } = useChatbotContext();
   return (
     <>
-      <div className="cursor-pointer">
-        <button
-          type="button"
-          className="inline-flex items-center justify-center fixed z-[888888] text-white rounded-full shadow-custom h-12 w-12 cursor-pointer hover:duration-200 hover:scale-105 bottom-4 right-4 hover:shadow-md hover:contrast-150"
-          onClick={() => setIsOpen(!isOpen)}
-          style={{
-            backgroundColor: brandColor ?? '#e5e5e5'
-          }}
-          aria-label={isOpen ? 'Close Chat' : 'Open Chat'}
-          title={isOpen ? 'Close Chat' : 'Open Chat'}
-        >
-          <div className="flex items-center justify-center w-full h-full">
-            {!isOpen ? (
-              <ChatIcon
-                className="w-6 h-6"
-                aria-hidden="true"
-              />
-            ) : (
-              <XMarkIcon
-                className="w-7 h-7"
-                aria-hidden="true"
-              />
-            )}
-          </div>
-        </button>
-      </div>
+      <button
+        type="button"
+        className="cursor-pointer inline-flex items-center justify-center fixed z-[888888] text-white rounded-full shadow-custom h-12 w-12 cursor-pointer hover:duration-200 hover:scale-105 bottom-4 right-4 hover:shadow-md hover:contrast-150"
+        onClick={() => setIsOpen(!isOpen)}
+        style={{
+          backgroundColor: brandColor ?? '#e5e5e5'
+        }}
+        aria-label={isOpen ? 'Close Chat' : 'Open Chat'}
+        title={isOpen ? 'Close Chat' : 'Open Chat'}
+      >
+        <div className="flex items-center justify-center w-full h-full">
+          {!isOpen ? (
+            <ChatIcon
+              className="w-6 h-6"
+              aria-hidden="true"
+            />
+          ) : (
+            <XMarkIcon
+              className="w-7 h-7"
+              aria-hidden="true"
+            />
+          )}
+        </div>
+      </button>
     </>
   );
 };
