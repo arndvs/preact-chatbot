8c12d59068bb6aa17e66138114eaa321a835f07c
Andrew <andrew@ripemetrics.com>
2024-04-12 12:53:18 -0700

    Update usePusher hook to include new Pusher key and chatApiUrl in authEndpoint

--- src/hooks/usePusher.ts
+++ src/hooks/usePusher.ts
@@ -18,7 +18,7 @@ export const usePusher = () => {
         //@ts-ignore
         window.Echo = new Echo({
           broadcaster: 'pusher',
-          key: 'ba4d144ab20fb212f010',
+          key: env === 'dev' ? 'ba4d144ab20fb212f010' : pusherConfig.key,
           cluster: pusherConfig.cluster,
           forceTLS: true,
           authEndpoint: `${chatApiUrl}/api/broadcasting/reputation`
