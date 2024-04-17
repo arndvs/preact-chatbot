231f9d44b5db5ed7c263cac3847d6e9c466d4825
Andrew <andrew@ripemetrics.com>
2024-04-17 13:54:16 -0700

    Refactor console.log statements and update island_name in ChatBotHeaderResetChatButton component

--- src/components/chat/chatbot/chatbot-header-container/chatbot-header-reset-chat-button.tsx
+++ src/components/chat/chatbot/chatbot-header-container/chatbot-header-reset-chat-button.tsx
@@ -35,7 +35,7 @@ const ChatBotHeaderResetChatButton = () => {
         session_id: cookie?.length && cookie[1],
         customer_store_id: cookie?.length && cookie[2],
         refresh: true,
-        island_name: formattedIslandName
+        island_name: formattedName
       });
 
       setCookie(
