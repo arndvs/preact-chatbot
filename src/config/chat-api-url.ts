0dccbea9326626aa42582fc45579b661f7f8d1fa
Aaron Davis <aaron@ripemetrics.com>
2024-05-22 12:12:23 -0700

    Update chat API URL based on environment

--- src/config/chat-api-url.ts
+++ src/config/chat-api-url.ts
@@ -2,7 +2,7 @@
 //   process.env.CHAT_API_URL ?? 'https://api.rmdevs.com';
 
 export const getChatApiUrl = (env: string | undefined) => {
-  return env === 'dev'
-    ? 'https://api.rmdevs.com'
-    : 'https://api.ripemetrics.com';
+  return env === null
+    ? 'https://api.ripemetrics.com'
+    : 'https://api.rmdevs.com';
 };
