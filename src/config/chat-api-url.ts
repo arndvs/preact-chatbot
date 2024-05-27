bab8c4f7eaca3ffef8329bd68d28d02d783753dd
Andrew <andrew@ripemetrics.com>
2024-05-27 09:25:23 -0700

    Refactor getChatApiUrl function to handle different environments

--- src/config/chat-api-url.ts
+++ src/config/chat-api-url.ts
@@ -1,5 +1,6 @@
 export const getChatApiUrl = (env: string | null) => {
-  return 'https://api.rmdevs.com';
-  // ? 'https://api.ripemetrics.com'
-  // : 'https://api.rmdevs.com';
+  // return 'https://api.rmdevs.com';
+  return env === null
+    ? 'https://api.ripemetrics.com'
+    : 'https://api.rmdevs.com';
 };
