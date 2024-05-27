20c2fa57517aa618eaa9ed83ce2fe124b4f3a255
Andrew <andrew@ripemetrics.com>
2024-05-27 09:38:45 -0700

    Refactor getChatApiUrl function to use the production API URL

--- src/config/chat-api-url.ts
+++ src/config/chat-api-url.ts
@@ -1,6 +1,6 @@
 export const getChatApiUrl = (env: string | null) => {
-  // return 'https://api.rmdevs.com';
-  return env === null
-    ? 'https://api.ripemetrics.com'
-    : 'https://api.rmdevs.com';
+  return 'https://api.ripemetrics.com';
+  // return env === null
+  //   ? 'https://api.ripemetrics.com'
+  //   : 'https://api.rmdevs.com';
 };
