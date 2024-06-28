643c40b51ce4d98be8d9097e98db8c2dfcc19d7a
Nic Last <nic@ripemetrics.com>
2024-06-28 13:53:25 -0700

    Refactor useInitialData hook to use islandType-specific cookies

--- src/config/chat-api-url.ts
+++ src/config/chat-api-url.ts
@@ -1,5 +1,5 @@
 export const getChatApiUrl = (env: string | null) => {
-  return 'https://api.rmdevs.com';
+  return 'https://api.ripemetrics.com';
   // return env === null
   //   ? 'https://api.ripemetrics.com'
   //   : 'https://api.rmdevs.com';
